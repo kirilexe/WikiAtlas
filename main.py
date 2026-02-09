c700c751efbc59891e37a12fbb62f579276d65ce
kirilexe <timeuknown@gmail.com>
2026-02-09 19:08:26 +0200

    made the links save in a csv file for the graph creation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import json
+import csv
 
 API_URL = "https://en.wikipedia.org/w/api.php"
 headers = {
@@ -8,20 +9,58 @@ headers = {
 }
 
 title = "Animal"
+filename = title + "_links.csv"
+with open(filename, 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Source', 'Target']) # header for the file
 
-params = {
-    "action": "query",
-    "format": "json",
-    "titles": title,
-    "prop": "links",
-    "pllimit": "max",
-}
+def get_links(title):
+    params = {
+        "action": "query",
+        "format": "json",
+        "titles": title,
+        "prop": "links",
+        "pllimit": "50",
+    }
+    try:
+        response = requests.get(API_URL, headers=headers, params=params)
+        data = response.json()
+        pages = data["query"]["pages"]
 
-response = requests.get(API_URL, headers=headers, params=params)
-data = response.json()
+        for page_id in pages: 
+            if "links" in pages[page_id]:
+                return pages[page_id]["links"] # make sure it actually returns something
+            else:
+                print(f"No links found for {title}")
+                return [] 
+    except:
+        return []
 
-pages = data["query"]["pages"]
+def save_connection(source, target):
+    with open(filename, 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow([source, target])
 
+# fetch the links for the title
+print("Fetching links for the title: {title}")
+layer1_links = get_links(title)
+
+for link in layer1_links:
+    l1_title = link["title"]
+    save_connection(title, l1_title)
+    print(f"Fetched link: {l1_title}")
+
+    print(f"  Fetching Layer 2: {l1_title}")
+    layer2_links = get_links(l1_title)
+    
+    for sublink in layer2_links:
+        l2_title = sublink["title"]
+        save_connection(l1_title, l2_title)
+
+
+
+"""
+# base logic 
 for page_id in pages:
     if "links" in pages[page_id]:
         print("Links from {title}") 
@@ -29,3 +68,7 @@ for page_id in pages:
             print(link["title"])
     else:
         print(f"No links found for {title}")
+"""
+
+
+
